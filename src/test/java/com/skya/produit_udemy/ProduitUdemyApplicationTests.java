15b29f65f06f3ea1731e4e6b633a71bfcf116d57
kokoe-yasmine-ashley-satchivi <kokoe-yasmine-ashley.satchivi@ens.uvsq.fr>
2025-07-30 23:31:23 +0200

    Lesson 9 : Test du CRUD

--- src/test/java/com/skya/produit_udemy/ProduitUdemyApplicationTests.java
+++ src/test/java/com/skya/produit_udemy/ProduitUdemyApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Date;
+import java.util.List;
 
 @SpringBootTest
 class ProduitUdemyApplicationTests {
@@ -21,8 +22,38 @@ class ProduitUdemyApplicationTests {
 
 	@Test
 	public void testCreateProduit() {
-		Produit prod = new Produit("PC Dell",2200.500,new Date());
-		produitRepository.save(prod);
+		Produit prod_create = new Produit("PC Dell",2200.500,new Date());
+		produitRepository.save(prod_create);
+	}
+
+	@Test
+	public void testFindProduit() {
+		Produit prod_find = produitRepository.findById(1L).get();
+		System.out.println(prod_find);
+	}
+
+	@Test
+	public void testUpdateProduit() {
+		Produit prod_update = produitRepository.findById(2L).get();
+		prod_update.setPrixProduit(20000.00);
+		produitRepository.save(prod_update);
+
+		System.out.println(prod_update);
+	}
+
+
+	@Test
+	public void testDeleteProduit() {
+		produitRepository.deleteById(2L);
+	}
+
+	@Test
+	public void testFindAllProduit() {
+		List<Produit> prod_all = produitRepository.findAll();
+
+		for (Produit produit : prod_all) {
+			System.out.println(produit);
+		}
 	}
 
 }
