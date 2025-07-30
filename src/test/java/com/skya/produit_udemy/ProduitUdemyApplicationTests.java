8dc8e944ea5b2b414cf995dbcf09e9af84eec961
kokoe-yasmine-ashley-satchivi <kokoe-yasmine-ashley.satchivi@ens.uvsq.fr>
2025-07-30 22:55:41 +0200

    lesson 8

--- src/test/java/com/skya/produit_udemy/ProduitUdemyApplicationTests.java
+++ src/test/java/com/skya/produit_udemy/ProduitUdemyApplicationTests.java
@@ -1,13 +1,28 @@
 package com.skya.produit_udemy;
 
+import com.skya.produit_udemy.entity.Produit;
+import com.skya.produit_udemy.repository.ProduitRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 @SpringBootTest
 class ProduitUdemyApplicationTests {
 
+//	@Test
+//	void contextLoads() {
+//	}
+
+	@Autowired
+	private ProduitRepository produitRepository;
+
+
 	@Test
-	void contextLoads() {
+	public void testCreateProduit() {
+		Produit prod = new Produit("PC Dell",2200.500,new Date());
+		produitRepository.save(prod);
 	}
 
 }
